9f257a27e22efc69f50d0764bdf8e2f469a854c5
zeeshansarwar2968 <zeeshansarwar2968@gmail.com>
2022-06-07 22:18:30 +0530

    [Zeeshan] : Updated testing for Regex User Registration

--- RegexTest/UserTests.cs
+++ RegexTest/UserTests.cs
@@ -6,7 +6,9 @@ namespace UserRegistrationTest
     [TestClass]
     public class UserTests
     {
-        RegexValidation regexValidation; 
+        //declaration of variable regexValidation of RegexValidation class
+        RegexValidation regexValidation;
+        
         [TestInitialize]
         public void InitValidationObj()
         {
@@ -16,11 +18,13 @@ namespace UserRegistrationTest
 
         //Method to test firstname function in user registration project
         [TestMethod]
-        public void FirstNameTest()
+        public void FirstNameTest_ShouldReturnTrue()
         {
+            //Arrange -- declare a firstName string variable 
+            string firstName = "Zeeshan";
 
             //Act -- basic logical construct for testing
-            var FirstName= regexValidation.ValidateFirstName("Zeeshan");
+            var FirstName= regexValidation.ValidateFirstName(firstName);
             
             //Assert --  test comparison with the standard values
             Assert.IsTrue(FirstName);   
@@ -29,11 +33,13 @@ namespace UserRegistrationTest
 
         //Method to test lastname function in user registration project
         [TestMethod]
-        public void LastNameTest()
+        public void LastNameTest_ShouldReturnTrue()
         {
+            //Arrange -- declare a lastName string variable 
+            string lastName = "Sarwar";
 
             //Act
-            var LastName = regexValidation.ValidateLastName("Sarwar");
+            var LastName = regexValidation.ValidateLastName(lastName);
 
             //Assert
             Assert.IsTrue(LastName);
@@ -42,11 +48,13 @@ namespace UserRegistrationTest
 
         //Method to test Email function in user registration project
         [TestMethod]
-        public void EmailTest()
+        public void EmailTest_ShouldReturnTrue()
         {
-
+            //Arrange -- declare an email string variable 
+            string email = "zeeshan.sarwar@fake.co.in";
+            
             //Act
-            var Email = regexValidation.ValidateEmail("zeeshan.sarwar@fake.co.in");
+            var Email = regexValidation.ValidateEmail(email);
 
             //Assert
             Assert.IsTrue(Email);
@@ -54,27 +62,98 @@ namespace UserRegistrationTest
         }
         //ValidatePhone
         [TestMethod]
-        public void PhoneNumberTest()
+        public void PhoneNumberTest_ShouldReturnTrue()
         {
+            //Arrange -- declare a number string variable 
+            string number = "91 2979243349";
 
             //Act
-            var phoneNumber = regexValidation.ValidatePhone("91 2979243349");
+            var phoneNumber = regexValidation.ValidatePhone(number);
 
             //Assert
             Assert.IsTrue(phoneNumber);
 
         }
 
+        //Validate Password
         [TestMethod]
-        public void PasswordTest()
+        public void PasswordTest_ShouldReturnTrue()
         {
+            //Arrange -- declare a passW string variable 
+            string passW = "sarwA1r@q";
 
             //Act
-            var password = regexValidation.ValidatePassword("sarwA1r@q");
+            var password = regexValidation.ValidatePassword(passW);
 
             //Assert
             Assert.IsTrue(password);
 
+        }
+
+        //Sad Test Cases(Test Cases Fail The Entry)
+        [TestMethod]
+        public void GivenUserFistName_ShouldReturnFalse()
+        {
+            //Arrange -- declare a firstName string variable 
+            string firstName = "zeeshan";
+            
+            //Act
+            bool FirstName = regexValidation.ValidateFirstName(firstName);
+            
+            //Assert
+            Assert.IsFalse(FirstName);
+        }
+
+        [TestMethod]
+        public void GivenUserLastName_ShouldReturnFalse()
+        {
+            //Arrange -- declare a lastName string variable 
+            string lastName = "sarwar";
+            
+            //Act
+            bool LastName = regexValidation.ValidateFirstName(lastName);
+            
+            //Assert
+            Assert.IsFalse(LastName);
+        }
+
+        [TestMethod]
+        public void GivenEmailId_ShouldReturnFalse()
+        {
+            //Arrange -- declare an email string variable 
+            string email = "zeesahn.sarwar.com";
+            
+            //Act
+            bool Email = regexValidation.ValidateEmail(email);
+            
+            //Assert
+            Assert.IsFalse(Email);
+        }
+
+        [TestMethod]
+        public void GivenMobileNumber_ShouldReturnFalse()
+        {
+            //Arrange -- declare a number string variable 
+            string number = "91 0863053941";
+            
+            //Act
+            bool phoneNumber = regexValidation.ValidatePhone(number);
+            
+            //Assert
+            Assert.IsFalse(phoneNumber);
+        }
+
+        [TestMethod]
+        public void GivenPassword_ShouldReturnFalse()
+        {
+            //Arrange -- declare a password string variable 
+            string password = "sarwa1r@q";
+            
+            //Act
+            bool Password = regexValidation.ValidatePassword(password);
+            
+            //Assert
+            Assert.IsFalse(Password);
         }
 
 
